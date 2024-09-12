83c66741a57702dcd7f22c0308a3c262f7273732
luiscarlos <luiscarlos8pe@gmail.com>
2024-09-12 08:48:34 -0300

    Criando campos de datas e botão buscar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,11 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
+
 
 @NgModule({
   declarations: [
@@ -45,7 +50,9 @@ import {MatInputModule} from '@angular/material/input';
     MatIconModule,
     MatChipsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [
     provideAnimationsAsync()
